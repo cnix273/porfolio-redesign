694bf9cf6e7b7aad5394942c1deb5656242a60bd
Cameron Nix <camerondeverenix@gmail.com>
2022-01-26 22:06:57 -0500

    added skills list

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./style.css";
+import { Row, Col } from "react-bootstrap";
 
-import { IoIosGlobe } from 'react-icons/io';
+// import { IoIosGlobe } from 'react-icons/io';
 import { IoCodeSlashOutline } from 'react-icons/io5';
 import { FaHtml5 } from 'react-icons/fa';
 import { FaCss3Alt } from 'react-icons/fa';
@@ -9,58 +10,68 @@ import { SiJavascript } from 'react-icons/si';
 import { FaReact } from 'react-icons/fa';
 import { FaNodeJs } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
+import { MdOutlineDesignServices } from 'react-icons/md';
+import { IoCreateOutline } from 'react-icons/io5';
 
 const Skills = () => {
     return (
         <div className="container-fluid" id="skillsContainer">
-            <div className="row cards">
-            <div className="profile-card" data-speed="2" id="webdev">
-                <IoCodeSlashOutline className="icon" size="5em"/>
+            <Row>
+                <Col xs={5} id="skills-list">
+                    <p id="skills-header">
+                        SKILLS
+                    </p>
+                    <p id="skills">
+                        Content Creation<br/>
+                        UI/UX Design<br/>
+                        Full Stack Development<br/>
+                        Creativity
+                    </p>
+                </Col>
+            </Row>
+            <Row className="cards">
+                <Col className="profile-card" data-speed="2" id="webdev">
+                    <IoCodeSlashOutline className="icon" size="5em"/>
 
-                    <h5 className="card-title">Web Developer</h5>
+                    <h5 className="card-title">Development</h5>
                     <p className="card-text">
                         Build dynamic, efficient applications with elegant, sleek &amp; captivating designs.
                     </p>
                     <h5 className="card-title">
                         Languages I speak:
                     </h5>
-                    <div className="col">
-                            <div className="skill-bar-title">
-                                <FaHtml5 size="3em"/>
-                                <p>HTML5</p>
-                            </div>
-                        </div>
-
-                        <div className="col">
-                            <div className="skill-bar-title">
-                                <FaCss3Alt size="3em"/>
-                                <p>CSS3</p>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="skill-bar-title">
-                                <SiJavascript size="3em"/>
-                                <p>JavaScript</p>
-                            </div>
-                        </div>
-                        <div className="col">
+                    <Row>
+                        <Col xs={4}>
                             <div className="skill-bar-title">
                                 <FaReact size="3em"/>
                                 <p>React</p>
                             </div>
-                        </div>
-                        <div className="col">
+                        </Col>
+                        <Col xs={4}>
                             <div className="skill-bar-title">
                                 <FaNodeJs size="3em"/>
                                 <p>NodeJS</p>
                             </div>
-                        </div>
-                        <div className="col">
+                        </Col>
+                        <Col xs={4}>
                             <div className="skill-bar-title">
-                                <FaGithub size="3em"/>
-                                <p>Git</p>
+                                <FaHtml5 size="3em"/>
+                                <p>HTML5</p>
                             </div>
-                        </div>
+                        </Col>
+                        <Col xs={6}>
+                            <div className="skill-bar-title">
+                                <FaCss3Alt size="3em"/>
+                                <p>CSS3</p>
+                            </div>
+                        </Col>
+                        <Col xs={6}>
+                            <div className="skill-bar-title">
+                                <SiJavascript size="3em"/>
+                                <p>JavaScript</p>
+                            </div>
+                        </Col>
+                    </Row>
                     <p className="card-text">
                         JavaScript, React, jQuery, HTML5, CSS3, Handlebars
                     </p>
@@ -75,11 +86,11 @@ const Skills = () => {
                         Terminal<br></br>
                         VSCode<br></br>
                     </p>
-                </div>
-                {/* <div className="profile-card" id="engineer">
-                    <IoCodeSlashOutline className="icon" size="5em"/>
-                    
-                    <h5 className="card-title">Software Engineer</h5>
+                </Col>
+                <Col className="profile-card" id="engineer">
+                    <IoCreateOutline className="icon" size="5em"/>
+                        
+                    <h5 className="card-title">Design</h5>
                     <p className="card-text">
                         Create practical &amp; secure server-side applications that meet all your business and personal needs.
                     </p>
@@ -99,12 +110,12 @@ const Skills = () => {
                         Application Design<br></br>
                         Database Architecting<br></br>
                     </p>
-                </div> */}
-            </div>
+                </Col>
+            </Row>
 
             {/* <br></br> */}
 
-            <div className="row">
+            {/* <div className="row">
                 <div className="col-md-10 offset-md-1">
                     <div className="row text-center languages">
     
@@ -147,7 +158,7 @@ const Skills = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
             {/* <br></br> */}
         </div>
