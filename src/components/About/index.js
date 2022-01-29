598f0f76d9bd615719e28b7db4d6a3149cc41b8c
Cameron Nix <camerondeverenix@gmail.com>
2022-01-28 23:06:45 -0500

    added about header and right border between columns

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,34 +4,32 @@ import { Row, Col } from "react-bootstrap";
 import "./style.css";
 import profPic from "../Assets/professional_pic.jpg";
 
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-
 export default class About extends Component {
-
     render() {
-        const {location } = this.props;
-
-        const timeout = {enter: 800, exit: 400 };
-
         return (
-            <TransitionGroup component="div" className="container-fluid App" id="aboutContainer">
-                <CSSTransition timeout={timeout} className="pageSlider" mountOnEnter={false}>
+            <section className="container-fluid" id="aboutContainer">
+                <p id="about-header">ABOUT</p>
                 <Row className="row">
-                    <Col xs={6} id="portfolio-picture">
+                    <Col xs={6} className="midline-border">
                         <img className="img-thumbnail" id="mainImage" src={profPic} alt="Cameron Nix headshot"/>
                         {/* <p>A solutions-driven software developer with a passion for creating beautiful &amp; scalable web applications built on JavaScript.</p> */}
                     </Col>
                     <Col xs={6} id="about-text">
-                        <p>An innovative &amp; solutions-driven software developer with a passion for creating beautiful, responsive &amp; scalable web applications.</p>
+                        <p>I am a bi-continental developer </p>
+                        <p>I am a creative designer and developer, who aims to work with small businesses and marginalized communities to bring their passions to life. I offer both design and development services of web applications or websites!</p>
+                        {/* <p>An innovative &amp; solutions-driven software developer with a passion for creating beautiful, responsive &amp; scalable web applications.</p>
                         {/* <p>A life-long learner who finds joy in problem solving.</p> */}
-                        <p>An enthusiastic &amp; driven UCLA graduate with a life-long affinity for architecting thoughtful, straightfoward solutions to complex problem.</p>
+                        {/* <p>An enthusiastic &amp; driven UCLA graduate with a life-long affinity for architecting thoughtful, straightfoward solutions to complex problem.</p>
                         <p>An innate communicator, team player &amp; leader whose inherent empathy fuels a strong desire to connect with, collaborate with &amp; learn from others.</p>
-                        <br/>
+                        <br/> */}
                         <p><a className="contactlinks" href="https://github.com/cnix273">https://github.com/cnix273</a> &emsp; &emsp; <a className="contactlinks" href="mailto:camerondeverenix@gmail.com">camerondeverenix@gmail.com</a></p>
                     </Col>
                 </Row>
-                </CSSTransition>
-            </TransitionGroup>
+                <Row id="pre-skills">
+                    <Col className="midline-border"/>
+                    <Col/>
+                </Row>
+            </section>
         );
     }
 };
